e615c79a62ba8d04e83e8a70adf8ddcc66ebd52f
TeraName <tera@email.com>
2026-03-15 14:49:57 +0530

    fix left right side field overflow buttons

--- lib/custom_button_model.dart
+++ lib/custom_button_model.dart
@@ -15,6 +15,7 @@ class CustomButton {
   double uiPosY;
   String swipeDir;
   double swipeDist;
+  String side; // 'left' or 'right'
 
   CustomButton({
     required this.id,
@@ -29,7 +30,20 @@ class CustomButton {
     this.uiPosY    = 0,
     this.swipeDir  = 'none',
     this.swipeDist = 100,
+    this.side      = 'right',
   });
+
+  Color get pressColor {
+    switch (name.toUpperCase()) {
+      case 'GAS':     return const Color(0xFF00C853);
+      case 'BRAKE':   return const Color(0xFFD50000);
+      case 'REVERSE': return const Color(0xFFFF6D00);
+      case 'FRONT':   return const Color(0xFF00B0FF);
+      case 'GEAR+':   return const Color(0xFF00C853);
+      case 'GEAR-':   return const Color(0xFFFF6D00);
+      default:        return const Color(0xFFFFFFFF);
+    }
+  }
 
   Map<String, dynamic> toJson() => {
     'id': id, 'name': name, 'icon': icon, 'isHold': isHold,
@@ -37,6 +51,7 @@ class CustomButton {
     'uiWidth': uiWidth, 'uiHeight': uiHeight,
     'uiPosX': uiPosX, 'uiPosY': uiPosY,
     'swipeDir': swipeDir, 'swipeDist': swipeDist,
+    'side': side,
   };
 
   factory CustomButton.fromJson(Map<String, dynamic> j) => CustomButton(
@@ -52,61 +67,51 @@ class CustomButton {
     uiPosY:    (j['uiPosY']   ?? 0.0).toDouble(),
     swipeDir:  j['swipeDir']  ?? 'none',
     swipeDist: (j['swipeDist'] ?? 100.0).toDouble(),
+    side:      j['side'] ?? 'right',
   );
-
-  // Button ke hisab se color
-  Color get pressColor {
-    switch (name.toUpperCase()) {
-      case 'GAS':     return const Color(0xFF00C853); // green
-      case 'BRAKE':   return const Color(0xFFD50000); // red
-      case 'REVERSE': return const Color(0xFFFF6D00); // orange
-      case 'FRONT':   return const Color(0xFF00B0FF); // blue
-      case 'GEAR+':   return const Color(0xFF00C853); // green
-      case 'GEAR-':   return const Color(0xFFFF6D00); // orange
-      default:        return const Color(0xFFFFFFFF); // white
-    }
-  }
 }
-
-// ignore: non_constant_identifier_names
 
 List<CustomButton> defaultButtons() => [
   CustomButton(
     id: 'brake', name: 'BRAKE', icon: 'pan_tool',
     isHold: true,
     touchX: 1933, touchY: 927,
-    uiWidth: 170, uiHeight: 200,
-    uiPosX: 10,   uiPosY: 160,
+    uiWidth: 130, uiHeight: 140,
+    uiPosX: 10, uiPosY: 160,
     swipeDir: 'none', swipeDist: 0,
+    side: 'left', // LEFT side
   ),
   CustomButton(
     id: 'gas', name: 'GAS', icon: 'speed',
     isHold: true,
     touchX: 2192, touchY: 850,
-    uiWidth: 170, uiHeight: 200,
-    uiPosX: 650,  uiPosY: 160,
+    uiWidth: 130, uiHeight: 140,
+    uiPosX: 650, uiPosY: 160,
     swipeDir: 'none', swipeDist: 0,
+    side: 'right', // RIGHT side
   ),
   CustomButton(
     id: 'reverse', name: 'REVERSE', icon: 'keyboard_arrow_up',
     isHold: false,
     touchX: 2244, touchY: 592,
-    uiWidth: 80,  uiHeight: 60,
-    uiPosX: 560,  uiPosY: 80,
+    uiWidth: 110, uiHeight: 80,
+    uiPosX: 560, uiPosY: 80,
     swipeDir: 'up', swipeDist: 250,
+    side: 'right',
   ),
   CustomButton(
     id: 'front', name: 'FRONT', icon: 'keyboard_arrow_down',
     isHold: false,
     touchX: 2244, touchY: 400,
-    uiWidth: 80,  uiHeight: 60,
-    uiPosX: 700,  uiPosY: 80,
+    uiWidth: 110, uiHeight: 80,
+    uiPosX: 700, uiPosY: 80,
     swipeDir: 'down', swipeDist: 250,
+    side: 'left',
   ),
 ];
 
 class ButtonStorage {
-  static const _key = 'custom_buttons_v5';
+  static const _key = 'custom_buttons_v6';
 
   static Future<List<CustomButton>> load() async {
     final prefs = await SharedPreferences.getInstance();
